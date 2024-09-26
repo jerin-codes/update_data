403c7dac0c423df7516cc203fae7a4e1da520650
Jerin Bolgatty <Jerin.James@bolgatty.nl>
2024-09-26 11:46:04 +0530

    More optimized code is added

--- main.py
+++ main.py
@@ -5,18 +5,20 @@ import pycountry
 import re
 
 def get_teamID(team):
-    if team == "Kannur Warriors FC":
+    if team == "Kannur Warriors FC" or team == "Kannur Warriors" or team== "Kannur":
         return 27
-    elif team == " KOCHI FORCA FC" :
+    elif team == " KOCHI FORCA FC" or team=="KOCHI FORCA" or team=="KOCHI":
         return 20
-    elif team == "Calicut FC":
+    elif team == "Calicut FC" or team =="Calicut":
         return 22
-    elif team == "THIRUVANANTHAPURAM KOMBANS":
+    elif team == "THIRUVANANTHAPURAM KOMBANS" or team =="THIRUVANANTHAPURAM":
         return 24
-    elif team == "Malappuram Football Club":
+    elif team == "Malappuram Football Club" or team=="Malappuram Football" or team =="Malappuram":
         return 21
-    elif team == "Thrissur Magic FC":
+    elif team == "Thrissur Magic FC" or team =="Thrissur Magic" or team =="Thrissur":
         return 25
+    else:
+        print("Hello world,",team)
 def get_country(country):
      try:
         if pycountry.countries.lookup(country):
@@ -76,11 +78,11 @@ def process_pdf_and_send_to_api_team1(pdf_path, team_name,index_one,index_two):
                 structured_data.append(player_data)
 
     # Send data to the API
-    url = "http://21.194.133.109:8005/api/update"
+    url = "http://20.193.132.108:8004/api/update_slk_players"
     headers = {
         "Accept": "application/json",
         "Content-Type": "application/json",
-        "Cookie": "BOOXpLZHc3R21jdHk2VE05TE9McDdreDNrai9QK203UVI0VHRsL2g2ZEFmZGRXNmVDUEJIZzNzSWtsWko2SWJtQjBycE52dERIVldyYUJMVzZQd0U1SjV0ODB4K0FWTkthUVVKVU1odURoZmI5RG5GNXVBUllmTDNMTWFXeUMiLCJtYWMiOiIyYjk0NGMwMDM5MDcyOWYyZmJmM2MzM2Y0MTVjZGY3NmM3NGEzZTEyZWI5NjU2NjY0NWYzNTllM2ExZDc3NDc3IiwidGFnIjoiIn0%3D; laravel_session=eyJpdiI6IjJObThFaHhjcDI1dHNIK0RDcXhNVWc9PSIsInZhbHVlIjoiS2h6bklVWjVSWnh4SjBiK1ZUY0I4Ty9BZDdvWExkVXQ5N0x6anZOeTJxQWVzb1FPMUFTdW40d2RCK3YyZmZXK3lWNEMwWE1kTnpvRUFkQUVYRzVOM1llaUhjNnBGOGhxUFUxS29RWTFRYjBTK05JWElVUGxXQW1WMjBYM1lSNmUiLCJtYWMiOiI2ZjgwMmVlYzgyMGY3MWFhZjc2MmVlOTZhZGI2N2RlYzI1ODhkZmMzYTc3YWNkOGE4MDkwN2I0N2FlYjI5NDE4IiwidGFnIjoiIn0%3D"
+        "Cookie": "XSRF-TOKEN=eyJpdiI6ImNrbEdxczZXYURSRU84NkZvT3ZpMWc9PSIsInZhbHVlIjoiR3BOOXpLZHc3R21jdHk2VE05TE9McDdreDNrai9QK203UVI0VHRsL2g2ZEFmZGRXNmVDUEJIZzNzSWtsWko2SWJtQjBycE52dERIVldyYUJMVzZQd0U1SjV0ODB4K0FWTkthUVVKVU1odURoZmI5RG5GNXVBUllmTDNMTWFXeUMiLCJtYWMiOiIyYjk0NGMwMDM5MDcyOWYyZmJmM2MzM2Y0MTVjZGY3NmM3NGEzZTEyZWI5NjU2NjY0NWYzNTllM2ExZDc3NDc3IiwidGFnIjoiIn0%3D; laravel_session=eyJpdiI6IjJObThFaHhjcDI1dHNIK0RDcXhNVWc9PSIsInZhbHVlIjoiS2h6bklVWjVSWnh4SjBiK1ZUY0I4Ty9BZDdvWExkVXQ5N0x6anZOeTJxQWVzb1FPMUFTdW40d2RCK3YyZmZXK3lWNEMwWE1kTnpvRUFkQUVYRzVOM1llaUhjNnBGOGhxUFUxS29RWTFRYjBTK05JWElVUGxXQW1WMjBYM1lSNmUiLCJtYWMiOiI2ZjgwMmVlYzgyMGY3MWFhZjc2MmVlOTZhZGI2N2RlYzI1ODhkZmMzYTc3YWNkOGE4MDkwN2I0N2FlYjI5NDE4IiwidGFnIjoiIn0%3D"
     }
 
     for player in structured_data:
@@ -96,5 +98,5 @@ def process_pdf_and_send_to_api_team1(pdf_path, team_name,index_one,index_two):
             print(f"Error sending data for jersey number {player['jersey_number']}: {e}")
 
 # Example usage
-process_pdf_and_send_to_api_team1(pdf_path="thrissur vs kannur.pdf",team_name="Kannur Warriors FC",index_one=2,index_two=4)#Scraping the first team players details from the pdf
-process_pdf_and_send_to_api_team1(pdf_path="thrissur vs kannur.pdf",team_name="Kannur Warriors FC",index_one=3,index_two=5)#Scraping the secong team players details from the pdf
+process_pdf_and_send_to_api_team1(pdf_path="malappuram vs calicut.pdf",team_name="Kannur Warriors FC",index_one=2,index_two=4)#Scraping the first team players details from the pdf
+process_pdf_and_send_to_api_team1(pdf_path="malappuram vs calicut.pdf",team_name="Kannur Warriors FC",index_one=3,index_two=5)#Scraping the secong team players details from the pdf
